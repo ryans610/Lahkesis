b7894041048b0de36ef2a93405909b66680bb475
ryans610 <ryans610@gmail.com>
2022-07-20 18:16:55 +0800

    sync

--- RyanJuan.Lahkesis/LahkesisExtensions/NextInt32.cs
+++ RyanJuan.Lahkesis/LahkesisExtensions/NextInt32.cs
@@ -5,13 +5,26 @@ namespace RyanJuan.Lahkesis
     public static partial class LahkesisExtensions
     {
 #if ZH_HANT
-#else
         /// <summary>
-        /// 
+        /// 傳回指定範圍內的隨機整數。
         /// </summary>
         /// <param name="random"></param>
         /// <param name="minValue"></param>
         /// <param name="maxValue"></param>
+        /// <returns></returns>
+#else
+        /// <summary>
+        /// Returns a random integer that is within a specified range.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="minValue">
+        /// The inclusive lower bound of the random number returned.
+        /// </param>
+        /// <param name="maxValue">
+        /// The exclusive upper bound of the random number returned.
+        /// <paramref name="maxValue"/> must be greater than
+        /// or equal to <paramref name="minValue"/>.
+        /// </param>
         /// <returns></returns>
 #endif
         public static int NextInt32(
@@ -27,12 +40,23 @@ namespace RyanJuan.Lahkesis
         }
 
 #if ZH_HANT
-#else
         /// <summary>
-        /// 
+        /// 傳回小於指定之最大值的非負值隨機整數。
         /// </summary>
         /// <param name="random"></param>
         /// <param name="maxValue"></param>
+        /// <returns></returns>
+#else
+        /// <summary>
+        /// Returns a non-negative random integer that is
+        /// less than the specified maximum.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="maxValue">
+        /// The exclusive upper bound of the random number
+        /// to be generated.
+        /// <paramref name="maxValue"/> must be greater than or equal to 0.
+        /// </param>
         /// <returns></returns>
 #endif
         public static int NextInt32(
@@ -47,9 +71,14 @@ namespace RyanJuan.Lahkesis
         }
 
 #if ZH_HANT
+        /// <summary>
+        /// 傳回非負值的隨機整數。
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
 #else
         /// <summary>
-        /// 
+        /// Returns a non-negative random integer.
         /// </summary>
         /// <param name="random"></param>
         /// <returns></returns>
