a71fefe061772e4ac4b87e205a394a4893cba85e
ryan <ryans610@gmail.com>
2022-07-25 00:43:20 +0800

    sync

--- RyanJuan.Lahkesis/pack.ps1
+++ RyanJuan.Lahkesis/pack.ps1
@@ -1,6 +1,13 @@
-﻿dotnet build --configuration Release
-dotnet build --configuration Release-zh-Hant
+﻿$name = "RyanJuan.Lahkesis"
 
-$xml = [Xml] (Get-Content .\RyanJuan.Lahkesis.csproj)
+Remove-Item -LiteralPath ".\obj" -Force -Recurse
+Remove-Item -LiteralPath ".\lib" -Force -Recurse
+
+$csproj = ".\$name.csproj"
+dotnet build $csproj -c Release
+dotnet build $csproj -c Release-zh-Hant
+
+$xml = [Xml] (Get-Content $csproj)
 $version = $xml.Project.PropertyGroup.Version
 echo "Target package version: $version"
+nuget pack ".\$name.nuspec" -Version $version
